70f6a41c2c3671b01948c38ed496058987f67a66
John Meehan <john.meehan1@gmail.com>
2017-01-10 17:13:38 +0000

    displays busstop information from api. enter a busstop numer to see arrivals.

---
+++ bus.js
@@ -0,0 +1,33 @@
+const endpoint = 'https://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid=609721&format=json';
+let busses = [];
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+function getBusStopArrivals(e){
+  let stopid = e.target.value;
+  console.log('Stopid: ', stopid);
+  const endpoint = `https://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid=${stopid}&format=json`;
+  const prom = fetch(endpoint, {method: 'get'})
+                  .then(blob => blob.json())
+                  .then(data => {
+    busses = [];
+    busses.push(...data.results)
+    displayBusses();
+  });
+
+}
+
+function displayBusses(){
+  let html = busses.map(bus => {
+    return `
+      <li>
+        <span class="name">${bus.route}, ${bus.destination}</span>
+        <span class="population">${bus.duetime}</span>
+      </li>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
+}
+
+searchInput.addEventListener('change', getBusStopArrivals);
+searchInput.addEventListener('keyup', getBusStopArrivals);
